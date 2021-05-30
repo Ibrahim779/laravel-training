0877a35a877920f62adeda586bf17e4ac6002175
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-05-31 00:27:40 +0200

    Update Authenticate.php

--- backend/app/Http/Middleware/Authenticate.php
+++ backend/app/Http/Middleware/Authenticate.php
@@ -15,6 +15,9 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            if ($request->routeIs('dashboard.*')){
+                return route('dashboard.login');
+            }
             return route('login');
         }
     }
