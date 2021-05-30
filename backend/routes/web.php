3130549847ea04ac0d7551be18c6157a919fe7da
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-05-31 00:29:19 +0200

    Create Set Lang Route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SetLang;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//set Lang
+Route::get('lang/{lang}', function ($lang){
+    session([
+        SetLang::LANG_KEY => $lang
+    ]);
+    return back();
+})->name('lang');
