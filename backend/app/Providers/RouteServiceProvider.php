9bc91a14be1dc9fc312a4f4228e1b88be77a95f1
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-05-31 00:25:10 +0200

    Create dashboard route

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -46,6 +46,12 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('dashboard')
+                ->middleware(   ['web', 'auth:admin'])
+                ->name('dashboard.')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/dashboard.php'));
         });
     }
 
---
+++ backend/routes/dashboard.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Dashboard\AuthController;
+use App\Http\Controllers\Dashboard\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::namespace('Dashboard')->group(function (){
+    Route::middleware('guest:admin')->group(function (){
+        Route::view('login', 'dashboard.auth.login')->name('loginForm');
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+    });
+    Route::get('/main', [DashboardController::class, 'index'])->name('main');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+});
