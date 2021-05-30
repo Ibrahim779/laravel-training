8ca2d74af3cdd925684fad1a25b4e240f6a6415c
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-05-31 00:27:37 +0200

    Update RedirectIfAuthenticated.php

--- backend/app/Http/Middleware/RedirectIfAuthenticated.php
+++ backend/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,6 +23,9 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
+                if ($guard == 'admin'){
+                    return redirect()->route('dashboard.main');
+                }
                 return redirect(RouteServiceProvider::HOME);
             }
         }
