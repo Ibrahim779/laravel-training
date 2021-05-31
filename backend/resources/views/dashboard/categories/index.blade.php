623625aea6fd9300cd9aeaf7a7ecf288867980da
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-05-31 19:28:30 +0200

    Create index.blade.php

---
+++ backend/resources/views/dashboard/categories/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.dashboard')
+@section('title', 'Categories')
+@section('content')
+    <div class="content-wrapper">
+        @include('dashboard.includes.header')
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <!-- /.card -->
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Categories</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name Arabic</th>
+                                    <th>Name English</th>
+                                    <th>Image</th>
+                                    <th>Control</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($categories as $category)
+                                <tr>
+                                    <td>{{$loop->iteration}}</td>
+                                    <td>
+                                        {{$category->name_ar}}
+                                    </td>
+                                    <td>
+                                        {{$category->name_en}}
+                                    </td>
+                                    <td>
+                                        <img style="width: 50px;height: auto" src="/{{$category->img}}" alt="category_img">
+                                    </td>
+                                    <td>
+                                        <a class="float-left mr-2" href="{{route('dashboard.categories.edit', $category->id)}}">
+                                            <button type="button" class="btn btn-primary btn-sm">edit</button>
+                                        </a>
+                                        <form method="post"  action="{{route('dashboard.categories.destroy', $category)}}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm">delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                            <a href="{{route('dashboard.categories.create')}}">
+                                <button type="button" class="pr-5 pl-5 btn btn-primary btn-md">Add</button>
+                            </a>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    </div>
+    @endsection
