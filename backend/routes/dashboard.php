a4f56ab1a59b1330189ca5d4d10c399701dd592f
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-05-31 19:28:52 +0200

    Update dashboard.php

--- backend/routes/dashboard.php
+++ backend/routes/dashboard.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Dashboard\AuthController;
+use App\Http\Controllers\Dashboard\CategoryController;
 use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,13 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('Dashboard')->group(function (){
-    Route::middleware('guest:admin')->group(function (){
-        Route::view('login', 'dashboard.auth.login')->name('loginForm');
-        Route::post('login', [AuthController::class, 'login'])->name('login');
-    });
-    Route::middleware('auth:admin')->group(function (){
-        Route::get('/main', [DashboardController::class, 'index'])->name('main');
-        Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-    });
+Route::middleware('guest:admin')->group(function (){
+    Route::view('login', 'dashboard.auth.login')->name('loginForm');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+Route::middleware('auth:admin')->group(function (){
+    Route::get('/main', [DashboardController::class, 'index'])->name('main');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::resource('categories', CategoryController::class);
 });
