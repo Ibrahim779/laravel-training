5d92742f07e028ce2b12636fff802c82e4032ddb
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-06-15 12:22:04 +0200

    Update AppServiceProvider.php

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('*', function ($view)
+        {
+            $view->with('cartItems', Cart::userCart()->get() );
+        });
     }
 }
