045c3c619faf6269cc0addf705c477434ea4862f
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-06-15 12:20:38 +0200

    Update Cart.php

--- backend/app/Models/Cart.php
+++ backend/app/Models/Cart.php
@@ -8,4 +8,42 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function scopeUserCart($query)
+    {
+        return $query->whereUserId(auth()->id());
+    }
+
+    public static function exist($productId)
+    {
+        if (self::userCart()->whereProductId($productId)->first())
+            return true;
+        return false;
+    }
+
+    public function getTotalPriceAttribute()
+    {
+        return $this->product->price * $this->count;
+    }
+
+    public static function getTotal()
+    {
+        $total = 0;
+        foreach (self::userCart()->get() as $cartItem) {
+            $total += $cartItem->totalPrice;
+        }
+        return $total;
+    }
 }
