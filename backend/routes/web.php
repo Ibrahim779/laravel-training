b6f11f2e4016bb1f6e3202f83c8f7a3bd3e629d2
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-06-15 12:24:40 +0200

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Site\AboutController;
 use App\Http\Controllers\Site\AuthController;
+use App\Http\Controllers\Site\CartController;
+use App\Http\Controllers\Site\ContactController;
 use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\NewsController;
+use App\Http\Controllers\Site\ProductController;
+use App\Http\Controllers\Site\WishlistController;
 use App\Http\Middleware\SetLang;
 use Illuminate\Support\Facades\Route;
 
@@ -16,12 +22,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::resource('products', ProductController::class)->only('index', 'show');
+Route::get('products/categories/{category}', [ProductController::class, 'getCategoryProducts'])
+    ->name('products.getCategoryProducts');
+Route::resource('news', NewsController::class)->only('index', 'show');
+Route::resource('about', AboutController::class)->only('index');
+Route::resource('contact', ContactController::class)->only('index');
+
 Route::middleware('auth')->group(function (){
-    Route::get('/', [HomeController::class, 'index'])->name('index');
+    Route::resource('cart', CartController::class)->only('index', 'store');
+    Route::resource('wishList', WishlistController::class)->only('index', 'store');
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::middleware('guest')->group(function (){
+Route::middleware('guest')->group(function () {
     Route::view('login', 'site.auth.login')->name('loginForm');
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::view('register', 'site.auth.register')->name('registerForm');
