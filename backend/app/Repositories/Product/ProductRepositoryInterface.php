14031934919bd2b3d7a0332c7bd3929328dc7d36
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-06-15 12:22:24 +0200

    Update ProductRepositoryInterface.php

--- backend/app/Repositories/Product/ProductRepositoryInterface.php
+++ backend/app/Repositories/Product/ProductRepositoryInterface.php
@@ -4,9 +4,15 @@
 namespace App\Repositories\Product;
 
 
+use App\Models\Category;
 use App\Repositories\BaseRepositoryInterface;
 
 interface ProductRepositoryInterface extends BaseRepositoryInterface
 {
 
+    public function getCategoryProducts($category);
+
+    public function getWithPagination();
+
+    public function getRelatedProducts($product);
 }
