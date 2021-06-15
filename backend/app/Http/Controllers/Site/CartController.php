4b0a78685f77b1e82c73c4b82ceeee8bbf6c5be0
ibrahim_ismail <ibrahim.ismail2022018@gmail.com>
2021-06-15 12:18:59 +0200

    Update CartController.php

--- backend/app/Http/Controllers/Site/CartController.php
+++ backend/app/Http/Controllers/Site/CartController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $cartItems = Cart::userCart()->get();
+        return view('site.cart.index', compact('cartItems'));
+    }
+
+    public function store()
+    {
+        $productId = request()->productId;
+        if (!Cart::exist($productId)) {
+            $this->saveData(new Cart, $productId);
+        }
+        return response()->json(['cartCount' => Cart::userCart()->count()]);
+    }
+
+    public function saveData(Cart $cart, $productId)
+    {
+        $cart->product_id = $productId;
+        $cart->user_id = auth()->id();
+        $cart->save();
+    }
+
 }
